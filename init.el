b3b5f37fd771f1b23fc0cdf535af1e122a99420b
Jose C. Massón <jose@gcoop.coop>
2018-10-12 00:48:20 -0300

    Remove unused function

--- init.el
+++ init.el
@@ -163,14 +163,6 @@ ARG."
   (back-to-indentation))
 
 
-(defun edit-current-file-as-root ()
-  "Edit the file that is associated with the current buffer as root."
-  (interactive)
-  (let ((filep (buffer-file-name)))
-    (kill-buffer (current-buffer))
-    (if filep (find-file (concat "/sudo::" filep))
-      (message "Current buffer does not have an associated file."))))
-
 ; ctags
 (defun crear-tags (dir-name)
   "Create tags file.
