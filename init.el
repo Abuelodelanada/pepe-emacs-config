bab6eed9d4e2218620e4257ed0ac31c343e18d33
Jose C. Massón <jose@gcoop.coop>
2019-02-23 16:44:48 -0300

    Add automode.el

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
 (package-initialize)
 (load "~/.emacs.d/init-packages")
 (load "~/.emacs.d/alias")
+(load "~/.emacs.d/automode")
 
 ;; Global settings
 (require 'iedit)
@@ -61,21 +62,6 @@
   "Major mode for editing Markdown files" t)
 
 
-
-(add-to-list 'auto-mode-alist '("\\.php" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl.php$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.adoc" . adoc-mode))
-(add-to-list 'auto-mode-alist '("\\.xml" . xml-mode))
-(add-to-list 'auto-mode-alist '("\\.xul" . xml-mode))
 
 ;; Hooks
 (add-hook 'window-setup-hook 'on-after-init)
