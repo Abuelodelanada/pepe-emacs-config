60805bc975a3364f8750cec3c3b911bb7f557057
Jose C. Massón <jose@gcoop.coop>
2019-02-28 22:22:55 -0300

    Change doom-modeline height

--- variables.el
+++ variables.el
@@ -26,7 +26,7 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
-(setq doom-modeline-height 30)
+(setq doom-modeline-height 28)
 (setq doom-modeline-bar-width 3)
 (setq doom-modeline-minor-modes t)
 (setq doom-modeline-checker-simple-format nil)
