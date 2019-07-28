e08729a0f98c1b526151caf7c711cbb3bc1f17c3
Jose C. Massón <jose@gcoop.coop>
2019-07-28 20:04:35 -0300

    Remove hook sqlformat-on-save-mode

--- hooks.el
+++ hooks.el
@@ -7,7 +7,6 @@
 ;(add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
 (add-hook 'js2-mode-hook 'jquery-doc-setup)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
-(add-hook 'sql-mode-hook 'sqlformat-on-save-mode)
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 (add-hook 'magit-mode-hook 'my-inhibit-global-nlinum-mode)
 (add-hook 'nlinum-mode-hook 'my-nlinum-mode-hook)
