1ba4c8ee40ac359c1b5b8e015be437706cca8ace
Jose C. Massón <jose@gcoop.coop>
2020-07-02 11:38:13 -0300

    Remove hooks

--- hooks.el
+++ hooks.el
@@ -5,8 +5,7 @@
 ;; Hooks
 (add-hook 'window-setup-hook 'on-after-init)
 ;(add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
-(add-hook 'js2-mode-hook 'jquery-doc-setup)
-(add-hook 'js2-mode-hook 'ac-js2-mode)
+
 
 (provide 'hooks)
 ;;; hooks ends here
