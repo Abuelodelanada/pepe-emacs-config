04219bd4671995273fee6ffcd9d6e69c39647050
Jose C. Massón <jose.masson@canonical.com>
2023-12-30 18:52:40 -0300

    modify git-gutter-fringe faces

--- use-package.el
+++ use-package.el
@@ -170,7 +170,11 @@
   :config
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+  :custom-face
+  (git-gutter-fr:added ((t (:inherit fringe :foreground "chartreuse"))))
+  (git-gutter-fr:deleted ((t (:inherit fringe :foreground "red"))))
+  (git-gutter-fr:modified ((t (:inherit fringe :foreground "gold")))))
 
 (use-package go-mode
   :defer t
